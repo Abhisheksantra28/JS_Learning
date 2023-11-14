66129b86ac7df18fe28fdc17ba44eb72d5591572
Abhishek Santra <abhisheksantra28@gmail.com>
2023-11-14 10:58:08 +0530

    Initial commit

---
+++ DataType_Conversion.js
@@ -0,0 +1,51 @@
+let score = "33";
+
+// const {score} =req.body
+// console.log(typeof (score))
+
+let valueINNUmber = Number(score);
+// console.log(typeof valueINNUmber)
+
+//"33" =>33
+//"33abs" =>NaN (Not a Number)
+//true => 1; false =>0
+//null=>0
+// undefined=>NaN
+
+let someNumber = 33;
+
+let stringNUmber = String(someNumber);
+// console.log(typeof stringNUmber)
+
+// .................operations...............
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello";
+let str2 = " abhi";
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true) =>1
+
+// console.log(+"") =>0
+
+
+
+
