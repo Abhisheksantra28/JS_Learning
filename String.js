e1eba19a27189b85a9dfec9c8da93cd609cabc88
Abhishek Santra <abhisheksantra28@gmail.com>
2023-11-14 13:37:50 +0530

    Strings

---
+++ String.js
@@ -0,0 +1,31 @@
+const name = "Abhi";
+const count = 50;
+
+// console.log( name + count)
+
+// console.log(`hello my name is ${name} and count is ${count}`);
+
+const gameName = new String("Abhishek-FC");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(1));
+// console.log(gameName.indexOf("b"));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(0, 4); // accepts negative values too
+console.log(anotherString);
+
+const newStringOne = "   abhi  ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+// .replace("what is to be replaced"," replaced with ")
+// .includes("somthing")
+
+// console.log(gameName.split("-")) =>coverted into array 
