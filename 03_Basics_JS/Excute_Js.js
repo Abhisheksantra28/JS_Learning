3f8e216fb46434b7aa80e5637990ff8ea4ad005e
Abhishek Santra <abhisheksantra28@gmail.com>
2023-12-04 12:33:18 +0530

    complete how does JS execute

---
+++ 03_Basics_JS/Excute_Js.js
@@ -0,0 +1,14 @@
+// Javascript Execution Context
+
+
+{} // Global EC in Nodejs and "window Object" in the browser
+
+// one more exceution context hai jis ka nam function exceution context
+//Eval in express not interested in this ab tak
+
+// execute in two phase ->
+// 1) Memory creation // creation phase
+// 2) Execution Phase 
+
+
+// need to watch the video how does javascript execute code 
