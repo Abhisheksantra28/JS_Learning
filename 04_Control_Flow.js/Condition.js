4f143cc3ca5a181137739e402f44a39a0ca5c6f6
Abhishek Santra <abhisheksantra28@gmail.com>
2023-12-06 11:49:36 +0530

    If_else condition

---
+++ 04_Control_Flow.js/Condition.js
@@ -0,0 +1,45 @@
+
+
+const temp = 41
+
+// if(2 == "2"){
+//     console.log("executed");
+// }
+
+// <,>,<=,>=, == , !=, ==, === , !===
+
+// if(temp < 50){
+//     console.log("temp is less than 50");
+// }
+// else{
+//     console.log("temp is greater than 50");
+// }
+
+const balance = 800
+
+// if(balance > 500) console.log("test"),
+// console.log("test2");  // nehi karna hai 
+
+
+
+// if(){
+
+// }
+// else if(){
+
+// }
+// else{
+
+// }
+
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+
