9e68c4b6c8df95806cd7bcddc447d14214598d97
Abhishek Santra <abhisheksantra28@gmail.com>
2023-12-12 23:10:21 +0530

    loops

---
+++ 05_Iterations.js/Loops.js
@@ -0,0 +1,28 @@
+// for
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`Outer loop ${i}`);
+
+//   for (let j = 0; j <= 10; j++) {
+//     console.log(`Innar loop  J: ${j} and inner i: ${i} `);
+//   }
+// }
+
+// break and continue
+
+// while
+// let i = 0;
+// while (i <= 10) {
+//   console.log(` value of i is ${i}`);
+//   i = i + 2;
+// }
+
+// do while
+
+// let score = 1;
+let score = 11;
+
+do {
+  console.log(`the score is ${score}`);
+  score++;
+} while (score <= 10);
